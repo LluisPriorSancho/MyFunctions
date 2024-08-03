3bf66e6db57e6b7a52e699240987ff5249ee621c
Lluis <lluis.prior@estudiantat.upc.edu>
2024-08-03 12:22:18 +0200

    reformat docstring

--- controllers.py
+++ controllers.py
@@ -6,18 +6,25 @@ import math as m
 class BaseFunctions:
     def __init__(self, up_range: float = m.pi, 
                        bot_range: float = -m.pi) -> None:
-        """
-        This class implements basic functions for the different controllers. Inputs common parameters for the controllers
+        """ This class implements basic functions for the different controllers. Inputs common parameters for the controllers
 
-            - up_range: Maximum value of the angle
-            - bot_range: Minimum value of the angle
+        Args:
+            up_range (float, optional): Maximum value of the angle limitation. Defaults to m.pi.
+            bot_range (float, optional): Minimum value of the angle limitation. Defaults to -m.pi.
         """
         self.up_range = up_range
         self.bot_range = bot_range
+        
 
     def set_limits_angle(self, new_up: float, new_bot: float) -> None:
-        """
-        Sets the range values for limiting angles
+        """ Sets the range values for limiting angles
+
+        Args:
+            new_up (float): New upper limit
+            new_bot (float): New lower limit
+
+        Raises:
+            ValueError: Upper limit must be bigger than the lower limit
         """
         if new_up < new_bot:
             raise ValueError(f"Error: new up value ({new_up}) must be greater than bot value ({new_bot}).")
@@ -25,8 +32,13 @@ class BaseFunctions:
         self.up_range = new_up
 
     def limit_range_angle(self, value: float) -> float:
-        """
-        Returns an angle limited to the range [bot_range, up_range]
+        """ Returns an angle limited to the range [bot_range, up_range]
+
+        Args:
+            value (float): Value to limit
+
+        Returns:
+            float: Limited value
         """
         while value < self.bot_range:
             value += 2 * m.pi
@@ -39,13 +51,13 @@ class PIDController(BaseFunctions):
                        kp: float = 1.0, 
                        ki: float = 0.0, 
                        kd: float = 0.0) -> None:
-        """
-        This class implements a PID controller. It allows also the control of an angle.
+        """This class implements a PID controller. It allows also the control of an angle.
 
-            - angle: Implements limits to the range [-pi, pi]. To modify limits call set_limits_angle()
-            - kp: Proportional gain
-            - ki: Integral gain
-            - kd: Derivative gain
+        Args:
+            angle (bool, optional): Implements limits to the range [-pi, pi]. To modify limits call set_limits_angle(). Defaults to False.
+            kp (float, optional): Proportional gain. Defaults to 1.0.
+            ki (float, optional): Integral gain. Defaults to 0.0.
+            kd (float, optional): Derivative gain. Defaults to 0.0.
         """
         super().__init__()  # Initialize the base class with default ranges
         self.kp = kp
@@ -58,12 +70,19 @@ class PIDController(BaseFunctions):
     @staticmethod
     def get_PID_params(controller_type: str = "P",
                        T: float = 0.0, 
-                       L: float = 0.0) -> tuple[float, float]:
-        """
-        Computes the Ziegler-Nichols PID parameters.
+                       L: float = 0.0) -> tuple[float, float, float]:
+        """ Computes the Ziegler-Nichols PID parameters.
 
-            - T
-            - L
+        Args:
+            controller_type (str, optional): type of controller: P, PI or PID. Defaults to "P".
+            T (float, optional): Defaults to 0.0.
+            L (float, optional): Defaults to 0.0.
+
+        Raises:
+            ValueError: T and L must be different
+
+        Returns:
+            tuple[float, float, float]: values of Kp, Ki and Kd
         """
         if T == L:
             raise ValueError(f"Error: T ({T}) must be different from L ({L}).")
@@ -78,13 +97,16 @@ class PIDController(BaseFunctions):
                    target: float, 
                    dt: float, 
                    bias: float = 0.0) -> float:
-        """
-        Computes one step of the controller.
+        """ Computes one step of the controller.
 
-            - state: Actual value of the controlled variable
-            - target: Goal value of the controlled variable
-            - dt: Time step
-            - bias: Offset added to the controlled action
+        Args:
+            state (float): Actual value of the controlled variable
+            target (float): Goal value of the controlled variable
+            dt (float): Time step
+            bias (float, optional): Offset added to the controlled action. Defaults to 0.0.
+
+        Returns:
+            float: New control action
         """
         error = target - state
         if self.angle:
@@ -98,11 +120,12 @@ class OnOffController(BaseFunctions):
     def __init__(self, angle: bool = False,
                        hysteresis: float = 0.0,
                        prev_control: bool = False) -> None:
-        """
-        This class implements an ON/OFF controller. It allows also the control of an angle. Returns True/False depending on the state of action
+        """ This class implements an ON/OFF controller. It allows also the control of an angle. 
 
-            - angle: Implements limits to the range [-pi, pi]. To modify limits call set_limits_angle()
-            - hysteresis: Value of the hysteresis
+        Args:
+            angle (bool, optional): Implements limits to the range [-pi, pi]. To modify limits call set_limits_angle(). Defaults to False.
+            hysteresis (float, optional): Value of the hysteresis. Defaults to 0.0.
+            prev_control (bool, optional): Previious control action. Defaults to False.
         """
         super().__init__()  # Initialize the base class with default ranges
         self.hysteresis = hysteresis
@@ -111,11 +134,14 @@ class OnOffController(BaseFunctions):
 
     def calc(self, state: float, 
                    target: float) -> bool:
-        """
-        Computes one step of the controller. 
+        """ Computes one step of the controller. 
 
-            - state: Actual value of the controlled variable
-            - target: Goal value of the controlled variable
+        Args:
+            state (float): Actual value of the controlled variable
+            target (float): Goal value of the controlled variable
+
+        Returns:
+            bool: Returns True/False depending on the state of action
         """
         if self.angle:
             state = self.limit_range_angle(state)
